c19047bcdee7c434bac2281c182c56be807459ef
JustM8 <gav.sqrt@gmail.com>
2023-05-23 09:24:11 +0300

    fix migrations

--- database/migrations/2022_11_17_141809_create_orders_table.php
+++ database/migrations/2022_11_17_141809_create_orders_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->foreignId('status_id')->constrained('order_statuses');
+            $table->foreignId('user_id')->constrained('users');
 
             $table->text('company_name');
             $table->string('phone',15);
