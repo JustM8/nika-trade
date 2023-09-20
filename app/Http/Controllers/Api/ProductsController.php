ce0eab9994d4cfb7aba3611965e1113f8bb8b42b
JustM8 <gav.sqrt@gmail.com>
2023-09-20 12:15:13 +0300

    [upd] api products change json data

--- app/Http/Controllers/Api/ProductsController.php
+++ app/Http/Controllers/Api/ProductsController.php
@@ -12,8 +12,21 @@ class ProductsController extends Controller
     public function index(Product $product)
     {
         $recommendedProducts = $product->recommendedProducts;
-        $product->thumbnail = $product->thumbnailUrl;
-        $product->obj_model = $product->objmodelUrl;
-        return response()->json(['cur'=>$product,'recommended'=>$recommendedProducts,'lang'=>App::currentLocale()]);
+
+        $data['cur'] =
+            [
+                'id'=> $product->id,
+                'category_id'=> $product->category_id,
+                'title'=> $product->title[App::currentLocale()],
+                'description'=> $product->description[App::currentLocale()],
+                'slug'=> $product->slug,
+                'SKU'=> $product->SKU,
+                'price'=> $product->price,
+                'discount'=> $product->discount,
+                'in_stock'=> $product->in_stock,
+                'thumbnail'=> $product->thumbnailUrl,
+            ];
+
+        return response()->json([$data]);
     }
 }
