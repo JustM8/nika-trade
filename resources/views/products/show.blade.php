f1aa68bdcc6bd8bc0c17dc6be49ca2a980793e84
imsashasho <imsashasho@gmail.com>
2023-09-25 13:11:23 +0300

    ok

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -32,29 +32,36 @@
                     </div>
                     <div class="product-page-item-info-wrap">
                         <div class="product-page-item-info">
-                            <div class="product-page-item-info__title-row"><span class="product-page-item-info__title-row-code text-14 text-black">Артикул:</span><span class="product-page-item-info__title-row-name text-14 text-black">Назва:</span><span class="product-page-item-info__title-row-price text-14 text-black">Ціна:</span><span class="product-page-item-info__title-row-quantity text-14 text-black">Кількість:</span><span class="product-page-item-info__title-row-blank"></span></div>
+                            <div class="product-page-item-info__title-row"><span class="product-page-item-info__title-row-code text-14 text-black">Артикул:</span>
+                            <span class="product-page-item-info__title-row-name text-14 text-black">Назва:</span>
+                            <span class="product-page-item-info__title-row-price text-14 text-black">Ціна:</span>
+                            <!-- <span class="product-page-item-info__title-row-quantity text-14 text-black">Кількість:</span> -->
+                            <span class="product-page-item-info__title-row-blank"></span></div>
                             <div class="product-page-list">
-                                <div class="product-page-item-info__row"><span class="product-page-item-info__row-code text-14 text-black">У 005</span><span class="product-page-item-info__row-name text-14 text-black">L-стійка Н=1220мм</span><span class="product-page-item-info__row-price text-14 text-black">810.00 грн</span><span class="product-page-item-info__row-quantity">
-                      <div class="buttons product-page-item-info__row-quantity-container">
-                        <div class="quantity product-page-item-info__row-quantity-num text-14 text-black">0</div>
-                        <div class="product-page-item-info__row-quantity-buttons">
-                          <button class="increment-btn product-page-item-info__row-quantity-increment text-14 text-black">
-                             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000" height="800px" width="800px" version="1.1" id="Layer_1" viewBox="0 0 330 330" xml:space="preserve">
-<path id="XMLID_224_" d="M325.606,229.393l-150.004-150C172.79,76.58,168.974,75,164.996,75c-3.979,0-7.794,1.581-10.607,4.394  l-149.996,150c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0l139.39-139.393l139.397,139.393  C307.322,253.536,311.161,255,315,255c3.839,0,7.678-1.464,10.607-4.394C331.464,244.748,331.464,235.251,325.606,229.393z"/>
-</svg>
-                          </button>
-                          <button class="decrement-btn product-page-item-info__row-quantity-decrement text-14 text-black">
-                             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000" height="800px" width="800px" version="1.1" id="Layer_1" viewBox="0 0 330 330" xml:space="preserve">
-<path id="XMLID_224_" d="M325.606,229.393l-150.004-150C172.79,76.58,168.974,75,164.996,75c-3.979,0-7.794,1.581-10.607,4.394  l-149.996,150c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0l139.39-139.393l139.397,139.393  C307.322,253.536,311.161,255,315,255c3.839,0,7.678-1.464,10.607-4.394C331.464,244.748,331.464,235.251,325.606,229.393z"/>
-</svg>
-                          </button>
-                        </div>
-                      </div></span>
+                                <div class="product-page-item-info__row">
+                                <span class="product-page-item-info__row-code text-14 text-black">У 005</span>
+                                <span class="product-page-item-info__row-name text-14 text-black">L-стійка Н=1220мм</span>
+                                <span class="product-page-item-info__row-price text-14 text-black">810.00 грн</span>
+                                <!-- <span class="product-page-item-info__row-quantity">
+                                    <div class="buttons product-page-item-info__row-quantity-container">
+                                        <div class="quantity product-page-item-info__row-quantity-num text-14 text-black">0</div>
+                                        <div class="product-page-item-info__row-quantity-buttons">
+                                            <button class="increment-btn product-page-item-info__row-quantity-increment text-14 text-black">
+                                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000" height="800px" width="800px" version="1.1" id="Layer_1" viewBox="0 0 330 330" xml:space="preserve">
+                                                    <path id="XMLID_224_" d="M325.606,229.393l-150.004-150C172.79,76.58,168.974,75,164.996,75c-3.979,0-7.794,1.581-10.607,4.394  l-149.996,150c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0l139.39-139.393l139.397,139.393  C307.322,253.536,311.161,255,315,255c3.839,0,7.678-1.464,10.607-4.394C331.464,244.748,331.464,235.251,325.606,229.393z"/>
+                                                </svg>
+                                            </button>
+                                            <button class="decrement-btn product-page-item-info__row-quantity-decrement text-14 text-black">
+                                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000" height="800px" width="800px" version="1.1" id="Layer_1" viewBox="0 0 330 330" xml:space="preserve">
+                                                    <path id="XMLID_224_" d="M325.606,229.393l-150.004-150C172.79,76.58,168.974,75,164.996,75c-3.979,0-7.794,1.581-10.607,4.394  l-149.996,150c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0l139.39-139.393l139.397,139.393  C307.322,253.536,311.161,255,315,255c3.839,0,7.678-1.464,10.607-4.394C331.464,244.748,331.464,235.251,325.606,229.393z"/>
+                                                </svg>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </span> -->
 
-                                    <div class="product-page-item-info__row-btn">
-
-
-                                        <form action="{{ route('cart.add', $product) }}" method="POST" class="form-inline">
+                                <div class="product-page-item-info__row-btn">
+                                    <form action="{{ route('cart.add', $product) }}" method="POST" class="form-inline">
                                         @csrf
                                         @method('post')
                                         <div class="form-group col-sm-3 mb-2">
@@ -62,23 +69,25 @@
                                                    name="product_count"
                                                    class="form-control"
                                                    id="product_count"
-{{--                                                   min="1"--}}
-{{--                                                   max="{{ $product->in_stock }}"--}}
+                                                    {{-- min="1"--}}
+                                                    {{--max="{{ $product->in_stock }}"--}}
                                                    value="1"
                                             >
                                         </div>
                                         <button type="submit" class="add-to-cart">
                                             <svg class="icon--cart" role="presentation">
-                                            <use xlink:href="#icon-cart"></use>
-                                        </svg><span class="text-14 text-white">Купити</span>
-                                    </button>
+                                                <use xlink:href="#icon-cart"></use>
+                                            </svg>
+                                            <span class="text-14 text-white">Купити</span>
+                                        </button>
                                     </form>
-</div>
                                 </div>
                             </div>
-                        </div><a class="product-page-item-btn text-14 text-black-100" href="">Завантажити каталог продукції</a>
+                        </div>
                     </div>
+                    <a class="product-page-item-btn text-14 text-black-100" href="">Завантажити каталог продукції</a>
                 </div>
+            </div>
                 <div class="product-page-info-wrap">
                     <div class="product-page-info-switch">
                         <button class="product-page-info-switch-item active" data-info="details"> <span class="text-14 text-black-100">Опис</span></button>
