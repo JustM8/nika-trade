12b3f597467250c55b0f6e8063cc94dd06cfbffa
imsashasho <imsashasho@gmail.com>
2023-09-25 13:41:35 +0300

    ok

--- resources/js/cart.js
+++ resources/js/cart.js
@@ -13,7 +13,40 @@ $(document).on('click', '.add-to-cart', function (e){
         },
         success: function (data){
             console.log('data',data);
-            $btn.parent().remove();
+            // $btn.parent().remove();
+
+            const popupFactory = ref => ({
+                open() {
+                  this.element.classList.add('modal-open');
+                },
+                close() {
+                  this.element.classList.remove('modal-open');
+                },
+                toggle() {
+                  this.element.classList.toggleClass('modal-open');
+                },
+                element: ref,
+              });
+              
+
+            const cartPopup = popupFactory(document.querySelector('.cart-overlay'));
+            const closeBtnRef = document.querySelector('.cart-close');
+
+            closeBtnRef.addEventListener('click', () => {
+                cartPopup.close();
+            });
+
+            const openCartPopup = () => {
+                const openBtnRef = document.querySelectorAll('.product-page-item-info__row-btn');
+                openBtnRef.forEach(el => {
+                  el.addEventListener('click', () => {
+                    cartPopup.open();
+                  });
+                });
+            };
+
+            openCartPopup();
+              
         },
         error: function (data){
             console.log('Error:',data);
