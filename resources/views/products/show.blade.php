8d3b4cd4a415ac260f8e3996751bace987d30240
imsashasho <imsashasho@gmail.com>
2023-09-25 13:58:04 +0300

    popup

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -61,12 +61,12 @@
                                 </span> -->
 
                                 <div class="product-page-item-info__row-btn">
-                                        <a data-route="{{ route('cart.add', $product) }}" type="submit" class="add-to-cart">
+                                        <button data-route="{{ route('cart.add', $product) }}" type="submit" class="add-to-cart">
                                             <svg class="icon--cart" role="presentation">
                                                 <use xlink:href="#icon-cart"></use>
                                             </svg>
                                             <span class="text-14 text-white">Купити</span>
-                                        </a>
+                                        </button>
                                 </div>
 
                             </div>
@@ -109,6 +109,77 @@
             </div>
         </div>
     </section>
+    <div class="cart-overlay">
+      <div class="cart-overlay-inner">
+        <div class="cart-wrap">
+            <button class="cart-close"><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
+                <path d="M1 11L11 1L1 11ZM11 11L1 1L11 11Z" fill="#C8102E"/>
+                <path d="M11 11L1 1M1 11L11 1L1 11Z" stroke="white" stroke-width="2"/>
+                </svg>
+            </button>
+            <div class="cart-title text-m text-black-100">Додано у кошик</div>
+            <div class="cart-list">
+                <div class="cart-list-item">
+                    <div class="cart-list-item-img"> <img src="" alt=""></div>
+                    <div class="cart-list-item-descr-wrap"> 
+                        <span class="cart-list-item-descr__name text-24 text-black-100">Стійка</span>
+                        <div class="cart-list-item-descr__row"> 
+                            <span class="cart-list-item-descr__row-size text-14 text-black-100">Розмір товару: </span>
+                            <span class="cart-list-item-descr__row-size text-14 text-black-100"></span>
+                        </div>
+                        <div class="cart-list-item-descr__row">
+                            <div class="cart-list-item-descr__row-code text-14 text-black-100">Артикул:</div>
+                            <div class="cart-list-item-descr__row-code text-14 text-black-100"></div>
+                        </div>
+                    </div>
+                    <span class="cart-list-item-descr__quantity">
+                        <div class="buttons cart-list-item-descr__quantity-btns">
+                            <div class="quantity cart-list-item-descr__quantity-num text-14 text-black">0</div>
+                            <div class="increment-btn cart-list-item-descr__quantity-increment text-14 text-black">
+                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000" height="800px" width="800px" version="1.1" id="Layer_1" viewBox="0 0 330 330" xml:space="preserve">
+                                    <path id="XMLID_224_" d="M325.606,229.393l-150.004-150C172.79,76.58,168.974,75,164.996,75c-3.979,0-7.794,1.581-10.607,4.394  l-149.996,150c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0l139.39-139.393l139.397,139.393  C307.322,253.536,311.161,255,315,255c3.839,0,7.678-1.464,10.607-4.394C331.464,244.748,331.464,235.251,325.606,229.393z"/>
+                                </svg>
+                            </div>
+                            <div class="decrement-btn cart-list-item-descr__quantity-decrement text-14 text-black">
+                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000" height="800px" width="800px" version="1.1" id="Layer_1" viewBox="0 0 330 330" xml:space="preserve">
+                                    <path id="XMLID_224_" d="M325.606,229.393l-150.004-150C172.79,76.58,168.974,75,164.996,75c-3.979,0-7.794,1.581-10.607,4.394  l-149.996,150c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0l139.39-139.393l139.397,139.393  C307.322,253.536,311.161,255,315,255c3.839,0,7.678-1.464,10.607-4.394C331.464,244.748,331.464,235.251,325.606,229.393z"/>
+                                </svg>
+                            </div>
+                        </div>
+                    </span>
+                    <span class="cart-list-item-descr__price text-18 text-black-100">777</span>
+                    <button class="cart-list-item-delete">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
+                            <path d="M1 11L11 1L1 11ZM11 11L1 1L11 11Z" fill="#C8102E"/>
+                            <path d="M11 11L1 1M1 11L11 1L1 11Z" stroke="white" stroke-width="2"/>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+            <div class="cart-descr"> 
+                <span class="cart-descr-title text-14 text-black-100">Діючі ціни можуть не збігатися з вказаними на сайті. Уточнюйте, будь ласка.</span>
+                <span class="cart-descr-price text-18 text-black-100"> Разом</span>
+                <span class="cart-descr-price-sum text-18 text-black-100"> </span>
+            </div>
+            <div class="cart-buttons-wrap"> 
+                <a class="cart-buttons__back" href="">
+                    <svg class="icon--arrow" role="presentation">
+                        <use xlink:href="#icon-arrow"></use>
+                    </svg>
+                    <span class="text-14 text-black-100">Продовжити покупки</span>
+                </a>
+                <a class="cart-buttons__cart" href="cart-page.html">
+                    <span class="text-14 text-white">Перейти у кошик</span>
+                    <svg class="icon--cart" role="presentation">
+                        <use xlink:href="#icon-cart"></use>
+                    </svg>
+                </a>
+            </div>
+        </div>
+      </div>
+      <!-- <div id="label"></div>
+      <div class="shopping-cart" id="shopping-cart"></div> -->
+    </div>
 
 
 @endsection
