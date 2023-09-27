5eba7e05e15ea1c212807fba5f53ed9d768cb719
yatsinoa <sashayatzino@gmail.com>
2023-09-27 16:16:14 +0300

    request2

--- resources/js/cart.js
+++ resources/js/cart.js
@@ -1,79 +1,155 @@
-import './bootstrap';
+import "./bootstrap";
 
-$(document).on('click', '.add-to-cart', function (e){
+$(document).on("click", ".add-to-cart", function (e) {
     e.preventDefault();
     let $btn = $(this);
 
     $.ajax({
-        url: $btn.data('route'),
-        type: 'POST',
-        dataType: 'json',
+        url: $btn.data("route"),
+        type: "POST",
+        dataType: "json",
         headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
         },
-        success: function (data){
-            console.log('data',data);
-            // $btn.parent().remove();
-
-            const popupFactory = ref => ({
+        success: function (data) {
+            const popupFactory = (ref) => ({
                 open() {
-                  this.element.classList.add('modal-open');
+                    this.element.classList.add("modal-open");
                 },
                 close() {
-                  this.element.classList.remove('modal-open');
+                    this.element.classList.remove("modal-open");
                 },
                 toggle() {
-                  this.element.classList.toggleClass('modal-open');
+                    this.element.classList.toggleClass("modal-open");
                 },
                 element: ref,
-              });
+            });
 
+            const cartPopup = popupFactory(
+                document.querySelector(".cart-overlay")
+            );
+            const closeBtnRef = document.querySelector(".cart-close");
 
-            const cartPopup = popupFactory(document.querySelector('.cart-overlay'));
-            const closeBtnRef = document.querySelector('.cart-close');
-
-            closeBtnRef.addEventListener('click', () => {
+            closeBtnRef.addEventListener("click", () => {
                 cartPopup.close();
             });
 
             const openCartPopup = () => {
-                const openBtnRef = document.querySelectorAll('.add-to-cart');
-                openBtnRef.forEach(el => {
-                  el.addEventListener('click', () => {
-                    cartPopup.open();
-                  });
+                const openBtnRef = document.querySelectorAll(".add-to-cart");
+                openBtnRef.forEach((el) => {
+                    el.addEventListener("click", () => {
+                        cartPopup.open();
+                    });
                 });
             };
 
             openCartPopup();
-
         },
-        error: function (data){
-            console.log('Error:',data);
-        }
-    })
-
+        error: function (data) {
+            console.log("Error:", data);
+        },
+    });
 });
 
+// let itemsCount = $(":input");
 
-$('input[type="number"]').on('keyup ', function() {
-  let $btn = $(this);
+// itemsCount.bind("keyup mouseup", function () {
+//     let $btn = $(this);
 
-  console.log($btn.val(),$btn.parent().data('row-id'))
+//     console.log($btn.val(), $btn.parent().data("row-id"));
 
-    $.ajax({
-        url: $btn.parent().data('route'),
-        type: 'POST',
-        dataType: 'json',
-        data: {count:$btn.val(),rowId:$btn.parent().data('row-id')},
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        },
-        success: function (data){
-            console.log('data',data);
-        },
-        error: function (data){
-            console.log('Error:',data);
-        }
-    })
+//     $.ajax({
+//         url: $btn.parent().data("route"),
+//         type: "POST",
+//         dataType: "json",
+//         data: { count: $btn.val(), rowId: $btn.parent().data("row-id") },
+//         headers: {
+//             "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+//         },
+//         success: function (data) {
+//             console.log("data", data);
+//         },
+//         error: function (data) {
+//             console.log("Error:", data);
+//         },
+//     });
+// });
+
+function updateQuantity($input) {
+    let $btn = $input;
+
+    console.log($btn.val(), $btn.parent().data("row-id"));
+
+    if ($btn.val() !== undefined && $btn.val() !== null && $btn.val() !== 0) {
+        $.ajax({
+            url: $btn.parent().data("route"),
+            type: "POST",
+            dataType: "json",
+            data: { count: $btn.val(), rowId: $btn.parent().data("row-id") },
+            headers: {
+                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+            },
+            success: function (data) {
+                console.log("data", data);
+            },
+            error: function (data) {
+                console.log("Error:", data);
+            },
+        });
+    } else {
+        console.log("0");
+    }
+}
+
+function incrementQuantity($input) {
+    let currentValue = parseInt($input.val()) || 0;
+    $input.val(currentValue + 1);
+    updateQuantity($input);
+}
+
+function decrementQuantity($input) {
+    let currentValue = parseInt($input.val()) || 0;
+    if (currentValue > 0) {
+        $input.val(currentValue - 1);
+        updateQuantity($input);
+    }
+}
+
+// Bind keyup and mouseup events to inputs
+$(document).on("keyup mouseup", ":input", function () {
+    updateQuantity($(this));
 });
+
+// Handle increment and decrement buttons
+$(document).on("click", ".cart-increment-btn", function (e) {
+    e.preventDefault();
+    let $input = $(".cart-list-item-descr__quantity-container__input-value");
+    incrementQuantity($input);
+});
+
+$(document).on("click", ".cart-decrement-btn", function (e) {
+    let $input = $(".cart-list-item-descr__quantity-container__input-value");
+    decrementQuantity($input);
+});
+
+// $('input[type="number"]').on('keyup ', function() {
+//   let $btn = $(this);
+
+//   console.log($btn.val(),$btn.parent().data('row-id'))
+
+//     $.ajax({
+//         url: $btn.parent().data('route'),
+//         type: 'POST',
+//         dataType: 'json',
+//         data: {count:$btn.val(),rowId:$btn.parent().data('row-id')},
+//         headers: {
+//             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+//         },
+//         success: function (data){
+//             console.log('data',data);
+//         },
+//         error: function (data){
+//             console.log('Error:',data);
+//         }
+//     })
+// });
--- resources/views/cart/parts/product_view_popup.blade.php
+++ resources/views/cart/parts/product_view_popup.blade.php
@@ -27,8 +27,21 @@
                             name="product_count"
                             class="cart-list-item-descr__quantity-container__input-value"
                         >
+
+                        <div class="cart-list-item-descr__quantity-buttons">
+                      <a type="submit"  class="cart-increment-btn product-page-item-info__row-quantity-increment text-14 text-black">
+                        <svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000" height="800px" width="800px" version="1.1" id="Layer_1" viewBox="0 0 330 330" xml:space="preserve">
+                          <path id="XMLID_224_" d="M325.606,229.393l-150.004-150C172.79,76.58,168.974,75,164.996,75c-3.979,0-7.794,1.581-10.607,4.394  l-149.996,150c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0l139.39-139.393l139.397,139.393  C307.322,253.536,311.161,255,315,255c3.839,0,7.678-1.464,10.607-4.394C331.464,244.748,331.464,235.251,325.606,229.393z"/>
+                        </svg>
+</a>
+                      <a type="submit" class= "cart-decrement-btn product-page-item-info__row-quantity-decrement text-14 text-black">
+                          <svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000" height="800px" width="800px" version="1.1" id="Layer_1" viewBox="0 0 330 330" xml:space="preserve">
+                            <path id="XMLID_224_" d="M325.606,229.393l-150.004-150C172.79,76.58,168.974,75,164.996,75c-3.979,0-7.794,1.581-10.607,4.394  l-149.996,150c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0l139.39-139.393l139.397,139.393  C307.322,253.536,311.161,255,315,255c3.839,0,7.678-1.464,10.607-4.394C331.464,244.748,331.464,235.251,325.606,229.393z"/>
+                          </svg>
+</a>
+</div>
                     </form>
-                    <div class="cart-list-item-descr__quantity-buttons">
+                    <!-- <div class="cart-list-item-descr__quantity-buttons">
                       <button  class="cart-increment-btn product-page-item-info__row-quantity-increment text-14 text-black">
                         <svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000" height="800px" width="800px" version="1.1" id="Layer_1" viewBox="0 0 330 330" xml:space="preserve">
                           <path id="XMLID_224_" d="M325.606,229.393l-150.004-150C172.79,76.58,168.974,75,164.996,75c-3.979,0-7.794,1.581-10.607,4.394  l-149.996,150c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0l139.39-139.393l139.397,139.393  C307.322,253.536,311.161,255,315,255c3.839,0,7.678-1.464,10.607-4.394C331.464,244.748,331.464,235.251,325.606,229.393z"/>
@@ -39,7 +52,7 @@
                             <path id="XMLID_224_" d="M325.606,229.393l-150.004-150C172.79,76.58,168.974,75,164.996,75c-3.979,0-7.794,1.581-10.607,4.394  l-149.996,150c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0l139.39-139.393l139.397,139.393  C307.322,253.536,311.161,255,315,255c3.839,0,7.678-1.464,10.607-4.394C331.464,244.748,331.464,235.251,325.606,229.393z"/>
                           </svg>
                       </button>
-                    </div>
+                    </div> -->
                   </div>
                 </span>
                 <span class="cart-list-item-descr__price text-18 text-black-100">  </span>
