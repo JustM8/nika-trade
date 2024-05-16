321cbc561b3680f0993b7ee19b7af56dc8172de2
yatsinoa <sashayatzino@gmail.com>
2024-05-16 14:51:39 +0300

    news fixed

--- resources/js/news.js
+++ resources/js/news.js
@@ -1,34 +1,34 @@
 import "./news/index";
 
-$(document).ready(function() {
-    var offset = 0;
+$(document).ready(function () {
+    var offset = 5;
     var limit = 5;
 
-    $(document).on('click', '#load-more-btn', function (e){
+    $(document).on("click", "#load-more-btn", function (e) {
         e.preventDefault();
         let $btn = $(this);
 
         $.ajax({
-            url: $btn.data('route'),
-            method: 'GET',
+            url: $btn.data("route"),
+            method: "GET",
             data: { offset: offset },
-            success: function(response) {
+            success: function (response) {
                 if (response.length > 0) {
-                    response.forEach(function(html) {
+                    response.forEach(function (html) {
                         // Додаємо HTML-рядок до контейнера новин
-                        $('#news-container').append(html);
+                        $("#news-container").append(html);
                     });
 
                     // Оновлюємо змінну зміщення (offset) для наступного запиту
                     offset += limit;
                 } else {
                     // Якщо не було отримано нових новин, сховати кнопку "Завантажити ще новини"
-                    $('#load-more-btn').hide();
+                    $("#load-more-btn").hide();
                 }
             },
-            error: function(xhr, status, error) {
+            error: function (xhr, status, error) {
                 console.error(error);
-            }
+            },
         });
     });
 });
