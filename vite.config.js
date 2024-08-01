71415125bdf5e69177c7de886cd9d836b6ab7b3b
Mate <gav.sqrt@gmail.com>
2024-08-01 12:47:11 +0300

    [upd] task 17272934 - summernote bug

--- vite.config.js
+++ vite.config.js
@@ -1,58 +1,52 @@
-import { defineConfig } from "vite";
-
-import laravel from "laravel-vite-plugin";
-
-import inject from "@rollup/plugin-inject";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import inject from '@rollup/plugin-inject';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default defineConfig({
     build: {
         cssSourceMap: true,
     },
     css: {
-        devSourcemap: true, // this one
+        devSourcemap: true,
     },
-    base: "/build/",
+    base: '/build/',
     plugins: [
         laravel({
             input: [
-                "resources/sass/app.scss",
-                "resources/sass/main.scss",
-                "resources/js/app.js",
-                "resources/js/images-preview.js",
-                "resources/js/images-preview-slider.js",
-                "resources/js/images-actions.js",
-                "resources/js/nikaModel.build.js",
-                "resources/js/summernote.js",
-                "resources/js/cart.js",
-                "resources/js/select.js",
-                "resources/js/cartPage.js",
-                "resources/js/catalogSingle.js",
-                "resources/js/catalog.js",
-                "resources/js/gallery.js",
-                "resources/js/contacts.js",
-                "resources/js/homepage.js",
-                "resources/js/news.js",
-                "resources/js/product-page.js",
-                "resources/js/services.js",
-                "resources/js/common.js",
-                "resources/js/model.js",
-                "resources/js/adminNews.js",
-                "resources/js/delete.js",
+                'resources/sass/app.scss',
+                'resources/sass/main.scss',
+                'resources/js/app.js',
+                'resources/js/images-preview.js',
+                'resources/js/images-preview-slider.js',
+                'resources/js/images-actions.js',
+                'resources/js/nikaModel.build.js',
+                'resources/js/summernote.js',
+                'resources/js/cart.js',
+                'resources/js/select.js',
+                'resources/js/cartPage.js',
+                'resources/js/catalogSingle.js',
+                'resources/js/catalog.js',
+                'resources/js/gallery.js',
+                'resources/js/contacts.js',
+                'resources/js/homepage.js',
+                'resources/js/news.js',
+                'resources/js/product-page.js',
+                'resources/js/services.js',
+                'resources/js/common.js',
+                'resources/js/model.js',
+                'resources/js/adminNews.js',
+                'resources/js/delete.js',
             ],
             refresh: true,
         }),
         inject({
-            // => that should be first under plugins array
-            $: "jquery",
-            jQuery: "jquery",
+            $: 'jquery',
+            jQuery: 'jquery',
         }),
+        commonjs(), // Додаємо плагін commonjs
     ],
     optimizeDeps: {
-        include: ["jquery", "chosen-js"], // Include all necessary dependencies
+        include: ['jquery', 'chosen-js'],
     },
-
-    // server: {
-    //     host: "https://nika-dev.smarto.com.ua/", // Замініть на домен вашого хостингу
-    //     port: 80, // Порт, на якому працює ваш сервер
-    // },
 });
